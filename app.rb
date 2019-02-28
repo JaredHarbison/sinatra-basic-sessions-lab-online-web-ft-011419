7c551cc4b93ca1c66a544acca53ec8389d1b649b
Jared Harbison <jared.harbison@gmail.com>
2019-02-28 17:23:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do 
+    enable :sessions
+    set :session_secret, "secret"
+  end 
+    
   get '/' do
+    erb :index
+  end 
 
+  post '/checkout' do 
+    @sessions = session 
+    @item = params[:item] 
+    @session[:item] = @item 
+    erb :checkout 
   end 
 end
